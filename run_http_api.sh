bd1b56810c4909bd5ad1039663c1eac67dce5ce9
yyu <magicyuyanchao@gmail.com>
2023-09-29 09:03:38 +0100

    make the docker work well

--- run_http_api.sh
+++ run_http_api.sh
@@ -9,19 +9,15 @@ echo $SCRIPTPATH
 PROJECTROOTPATH="$(dirname "$SCRIPTPATH")"
 echo $PROJECTROOTPATH
 
-#pyenv install 3.7.9
 pyenv local 3.7.9
 
 python -m venv .venv
 
-#make install-full
 
 source ${SCRIPTPATH}/.venv/bin/activate
 "${SCRIPTPATH}"/.venv/bin/pip3 install --upgrade setuptools pip
 
-## upcoming issue with pip3 in Oct 2020
-## Stick with rasa 1.10.14 -- using rasa2.0 wouldn't build in docker
-"${SCRIPTPATH}"/.venv/bin/pip3 install rasa==3.0
+"${SCRIPTPATH}"/.venv/bin/pip3 install rasa
 #
 ## 30 Nov 2020 ConvERT model available from alternative source
 ## but not yet specified in pipeline (Jan 2021)
@@ -29,11 +25,8 @@ source ${SCRIPTPATH}/.venv/bin/activate
 
 ## As at Nov 2020 ConvERT model no longer available
 "${SCRIPTPATH}"/.venv/bin/pip3 install rasa[spacy]
-"${SCRIPTPATH}"/.venv/bin/python3 -m spacy download en_core_web_md
-"${SCRIPTPATH}"/.venv/bin/python3 -m spacy link en_core_web_md en
+"${SCRIPTPATH}"/.venv/bin/python3 -m spacy download en_core_web_sm
+"${SCRIPTPATH}"/.venv/bin/python3 -m spacy link en_core_web_sm en
 
-#rasa data convert nlu -f yaml --data="${SCRIPTPATH}"/src/data --out="${SCRIPTPATH}"/src/data
-#rasa train nlu --out /scratch/Alana2018/SPRING/main/spring-alana/rasa/saved_models
-# by default, rasa will store latest trained model in sub-directory 'models'
-rasa train
-
+source ${SCRIPTPATH}/.venv/bin/activate
+rasa run --enable-api --cors "*" --debug --endpoints "endpoints.yml" --log-file "out.log" --debug
